d42e8d9639c5bcb4e8d9b3f9566d001d32b5a5f7
Ato Sugiharto <atosoegiharto@gmail.com>
2024-01-04 11:36:22 +0700

    update models

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect } from "react";
+import "./App.css";
+import * as faceapi from "face-api.js";
 
 function App() {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  // LOAD FROM USEEFFECT
+  useEffect(() => {
+    startVideo();
+    videoRef && loadModels();
+  }, []);
+
+  // OPEN YOU FACE WEBCAM
+  const startVideo = () => {
+    navigator?.mediaDevices
+      ?.getUserMedia({ video: true })
+      .then((currentStream) => {
+        videoRef.current.srcObject = currentStream;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  // LOAD MODELS FROM FACE API
+
+  const loadModels = async () => {
+    await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+    await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+    await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+    await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+
+    // Check if ageGenderNet is available in the faceapi.nets object
+    if (faceapi.nets.ageGenderNet) {
+      await faceapi.nets.ageGenderNet.loadFromUri("/models");
+    } else {
+      console.error("ageGenderNet not found in faceapi.nets");
+    }
+
+    faceMyDetect();
+  };
+
+  const faceMyDetect = () => {
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+
+      // DRAW YOU FACE IN WEBCAM
+      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(
+        videoRef.current
+      );
+      faceapi.matchDimensions(canvasRef.current, {
+        width: 940,
+        height: 650,
+      });
+
+      const resized = faceapi.resizeResults(detections, {
+        width: 940,
+        height: 650,
+      });
+
+      faceapi.draw.drawDetections(canvasRef.current, resized);
+      // faceapi.draw.drawFaceLandmarks(canvasRef.current,resized)
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
+    }, 1000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myapp">
+      <h1>Face Detection</h1>
+      <div className="appvide">
+        <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
+      </div>
+      <canvas ref={canvasRef} width="940" height="650" className="appcanvas" />
     </div>
   );
 }
